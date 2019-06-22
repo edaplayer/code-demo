94bc85695abf89fb4827f73bf7a86f3a206f3d98
tangfuyuan <tangfuyuan@szroco.com>
2019-06-22 15:51:32 +0800

    Add makefile and main.c

---
+++ make/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* #include "list.h" */
+
+void fun(int a)
+{
+    /* printf("hello world"); */
+}
+
+void fun2(int a)
+{
+    /* printf("hello world"); */
+}
+
+int main()
+{
+    int a = 1234;
+    /* int b =123; */
+    fun2(a);
+}

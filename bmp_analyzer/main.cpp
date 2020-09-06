21e0bc188f85517c61a99955337c5e534fc206db
edaplayer <edaplayer@163.com>
2020-09-06 01:34:46 -0700

    bmp_analyzer: Add print_bmpinfoheader

--- bmp_analyzer/main.cpp
+++ bmp_analyzer/main.cpp
@@ -26,7 +26,6 @@ union _dword {
  unsigned int all;
 };
 
-
 struct TbitmapHeader {
     _word bfType;
     _dword bfSize;
@@ -49,47 +48,84 @@ struct TbitmapHeader {
 
 void print_hex_array(char *array, unsigned int size) {
      unsigned int i;
-     printf("%lu: ", array);
-     for (i=0; i<size; i++) {
-         printf("%2.2x",array[i]);
-     }
-     printf(" (%db)\n",size);
+	 printf("%lu: ", (long unsigned int)array);
+	 printf("size = %d:", size);
+		 printf("%02X", (char)array[0]);
+		 printf("%02X", (char)array[1]);
+		 printf("%02X", (char)array[2]);
+		 printf("%02X", (char)array[3]);
+
+	 // for (i=0; i<size; i++) {
+		 // printf("%02X", (char)array[i]);
+	 // }
+	 printf(" (%db)\n",size);
 }
 
 void print_bitmapHeader(struct TbitmapHeader *p) {
-    printf("bfType ", p->bfType);
-    print_hex_array(p->bfType.array, sizeof(p->bfType.array));
-    printf("bfSize ",p->bfSize);
-    print_hex_array(p->bfSize.array, sizeof(p->bfSize.array));
-    printf("RSVD1 %4.4x\nRSVD2 %4.4x - ",p->bfReserved1, p->bfReserved2);
-    print_hex_array(p->bfReserved2.array, sizeof(p->bfReserved2.array));
-    printf("Offset %d[%2.2x] - ",p->bfOffBits,p->bfOffBits);
-    print_hex_array(p->bfOffBits.array, sizeof(p->bfOffBits.array));
-    // ---
-    printf("biSize %4.4x - ", p->biSize);
-    print_hex_array(p->biSize.array, sizeof(p->biSize.array));
-    printf("biWidth %4.4x - ", p->biWidth);
-    print_hex_array(p->biWidth.array, sizeof(p->biWidth.array));
+	printf("bfType ");
+	print_hex_array(p->bfType.array, sizeof(p->bfType.array));
+	printf("bfSize ");
+	print_hex_array(p->bfSize.array, sizeof(p->bfSize.array));
+	printf("RSVD1 %4.4x\nRSVD2 %4.4x - ", p->bfReserved1.all, p->bfReserved2.all);
+	print_hex_array(p->bfReserved2.array, sizeof(p->bfReserved2.array));
+	printf("Offset %d[%08x] - ", p->bfOffBits.all, p->bfOffBits.all);
+	print_hex_array(&p->bfOffBits.array[0], sizeof(p->bfOffBits.array));
+	// ---
+	printf("biSize %4.4x - ", p->biSize.all);
+	print_hex_array(p->biSize.array, sizeof(p->biSize.array));
+	printf("biWidth %4.4x - ", p->biWidth.all);
+	print_hex_array(p->biWidth.array, sizeof(p->biWidth.array));
 }
 
 void print_bmpfileheader(BITMAPFILEHEADER *p) {
-	printf("bfType ", p->bfType);
-	printf("bfSize ", p->bfSize);
-	printf("bfOffBits ", p->bfOffBits);
+	char *pc = (char *)&p->bfType;
+	printf("file header:\n");
+	printf("bfType: %c%c (char)\n", *pc, *(pc+ 1));
+	printf("bfSize: %d (whole bmp flie size)\n", p->bfSize);
+	printf("bfOffBits: %d (decimal) bytes\n", p->bfOffBits); printf("\n");
 }
 
-struct st {
-	_word array;
-} __attribute__((packed));
+void print_bmpinfoheader(BITMAPINFOHEADER *p) {
+    printf ("biSize(info)    = %d\n", p->biSize);
+    printf ("biWidth         = %d\n", p->biWidth);
+    printf ("biHeight        = %d\n", p->biHeight);
+    printf ("biPlanes        = %d (must be 1)\n", p->biPlanes);
+    printf ("biBitCount      = %d (per pixel take bits)\n", p->biBitCount);
+    printf ("biCompression   = %d\n", p->biCompression);
+    printf ("biSizeImage     = %d\n", p->biSizeImage);
+    printf ("biXPelsPerMeter = %d\n", p->biXPelsPerMeter);
+    printf ("biYPelsPerMeter = %d\n", p->biYPelsPerMeter);
+    printf ("biClrUsed       = %d\n", p->biClrUsed);
+    printf ("biClrImportant  = %d\n", p->biClrImportant);
+
+	int color_table_size = 0;
+	switch (p->biBitCount)
+	{
+	case 1:
+	case 2:
+	case 4:
+	case 8:
+		color_table_size = p->biClrUsed==0 ? (1<<(p->biBitCount+2)) : (p->biClrUsed)<<2;
+		break;
+	case 24:
+	case 32:
+		color_table_size = 0;
+		break;
+	default:
+		printf("Invaild biBitCount!\n");
+	}
+
+	if(p->biClrUsed != 0)
+		color_table_size = (p->biClrUsed) * 4;
+
+	printf ("color table size(RGBQUAD) = %d\n", color_table_size);
+}
 
 int main(int argc, char *argv[]) {
 
     int fd;
+	int i;
 	u8 array[sizeof(BITMAPHEADER)] = {0};
-
-	st testa = {0xff};
-
-	printf("st->arryay", testa.array);
 
 	BITMAPHEADER* pbmpheader = (BITMAPHEADER *)&array;
 	BITMAPFILEHEADER* pbfheader = &pbmpheader->fileheader;
@@ -103,14 +139,21 @@ int main(int argc, char *argv[]) {
 
 	read(fd, array, sizeof(BITMAPHEADER));
 
-	printf("sizeof BITMAPHEADER = %d\n", sizeof(BITMAPHEADER));
+	printf("%2x",(char)array[0]);
 
-	print_bmpfileheader(pbfheader);
-
+	printf("Readed %ld.\n", sizeof(TbitmapHeader));
+	for (i=0; i<sizeof(TbitmapHeader); i++ ) {
+		printf("%2.2x", array[i]);
+		if (i%2 == 1) printf(" ");
+	}
     printf("\n");
-
     close(fd);
 
+	// print_bitmapHeader((struct TbitmapHeader *)array);
+
+    printf("\n");
+	print_bmpfileheader(pbfheader);
+	print_bmpinfoheader(pbiheader);
 
     return 0;
 }
--- bmp_analyzer/main.i.d
+++
@@ -1,3 +0,0 @@
-main.o: main.cpp wintypes.h
-
-wintypes.h:

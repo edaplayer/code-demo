94595a2400ecaf3af5fd0fd916fed093bb9a7076
tangfuyuan <tangfuyuan@justtide.com>
2020-09-27 12:04:27 +0800

    Add test_pointer_array

--- test_pointer/main.c
+++ test_pointer/main.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <assert.h>
 
+#if 0
 int sum(int n) {
 	return (short*)n[(char(*)[n+1])0] - (short*)0;
 }
@@ -70,15 +71,15 @@ int test_short(void)
 int	test_begin(void)
 {
 	int n = 4;
-	/* char *begin = 0;
-	   char (*p)[n + 1] = (char (*)[n + 1]) begin;
-	   char *end = (char *)p[n];
-	   printf("begin = %d\n", begin);
-	   printf("end = %d\n", end);
-	   printf("%d\n", (end - begin) >> 1); */
+	char *begin = 0;
+	char (*p)[n + 1] = (char (*)[n + 1]) begin;
+	char *end = (char *)p[n];
+	printf("begin = %d\n", begin);
+	printf("end = %d\n", end);
+	printf("%d\n", (end - begin) >> 1);
 
 	/* 另一种计算方式 */
-	/* printf("%d\n", (short*)end - (short *)begin ); */
+	printf("%d\n", (short*)end - (short *)begin );
 }
 
 int test_matrix(void)
@@ -103,10 +104,65 @@ int test_matrix(void)
 	printf("The value is: %d\n", pmatrix[n]);//20, 相当于n*(n+1)
 }
 
+
+int test_array_pointer(void)
+{
+	char (*p1) [20]; //数组指针
+	char *p2 [20]; //指针数组
+	printf("Size of array pointer is: %d\n", sizeof(*p1));
+	printf("Size of pointer array is: %d\n", sizeof(*p2));
+	printf("*p2 is %p\n", (p2));
+	printf("*p2 is %p\n", (*p2));
+	printf("*p2 is %d\n", *(p2+1));
+	printf("*p2 is %d\n", (p2[1]));
+}
+#endif
+
+int print_pptr(char **pptr)
+{
+	printf("**(pptr) = *pptr[0], is %d\n", *pptr[0]);
+	printf("**(pptr+1) = *pptr[1], is %d\n", *pptr[1]);
+}
+
+int test_pointer_array(void)
+{
+	char a = 10;
+	char b = 20;
+	char *p [] = {//指针数组
+		&a,
+		&b
+	};
+
+	printf("Size of pointer array is: %ld\n", sizeof(*p));
+	printf("*p is %p\n", (p));
+
+	printf("*p[0] is %p\n", (p[0]));//第一个元素，是指针
+	printf("*p is %p\n", (*p));//数组第一个元素的另一种表示方法
+
+	printf("**p is %d\n", **(p));//相当于*p[0]
+	printf("*(p[0]) is %d\n", *(p[0]));//相当于*p[0]
+
+	printf("**(p+1) is %d\n", **(p+1));
+	printf("*(p[1]) is %d\n", *(p[1]));
+
+	print_pptr(p);
+
+
+	int matrix[3][10];
+	matrix[1][5] = 999;
+	int (*pm) [10] =  matrix;
+
+	//以下三种表示方法都是等效的
+	printf("matrix[1][5] is %d\n", *( *(matrix+1) + 5 ));
+	printf("*(*(pm+1)+5) is %d\n", *( *(pm+1) + 5 ));
+	printf("*(pm[1]+5) is %d\n", *( pm[1] + 5 ));
+
+}
+
 int main(int argc, char *argv[])
 {
 	/* test_matrix(); */
-	char a[5];
-	printf("%d\n", sizeof(a));
+	/* test_array_pointer(); */
+	test_pointer_array();
 	return 0;
 }

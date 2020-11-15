5812ac4ff311c6748242312ced8580ef379c1528
edaplayer <edaplayer@163.com>
2020-11-15 04:42:29 -0800

    nowcoder: Optimize huawei test3 code

--- nowcoder/huawei/test3/test.cpp
+++ nowcoder/huawei/test3/test.cpp
@@ -2,11 +2,11 @@
 #include <vector>
 #include <cctype>
 
-using namespace std; vector<string> keymap = {" ", ",.", "abc", "def", "ghi",
-						 "jkl", "mno", "pqrs", "tuv", "wxyz"};
+using namespace std;
 
-int main()
-{
+vector<string> keymap = {" ", ",.", "abc", "def", "ghi",
+	"jkl", "mno", "pqrs", "tuv", "wxyz"};
+int main() {
 	int n = 0;
 	string s;
 	int mode = 0;// 0: digital, 1: english
@@ -19,14 +19,12 @@ int main()
 			{
 				mode = ~mode;
 			} else {
-				if(mode == 0)
+				if(isdigit(s[i]))
 				{
-					if(isdigit(s[i]))
+					if(mode == 0)
 					{
 						cout << s[i];
-					}
-				} else {
-					if (isdigit(s[i])) {
+					} else {
 						int key = s[i] - '0';
 						int j = 0;
 						while(s[i + j] != '/' && s[i + j] - '0' == key && s[i + j] != '#')
@@ -34,13 +32,13 @@ int main()
 							j++;
 						}
 						int key_shift = (j-1)%(keymap[key].length());
-						printf("%c", keymap[key][key_shift]);
+						cout << keymap[key][key_shift];
 						i += (j-1);
 					}
 				}
 			}
 		}
 		cout << endl;
-		return 0;
 	}
+	return 0;
 }

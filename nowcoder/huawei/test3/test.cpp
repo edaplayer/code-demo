e1cace41e388bb7e3646b39f03578783b8c3e6a8
edaplayer <edaplayer@163.com>
2020-11-16 07:34:16 -0800

    nowcoder: Optimize test3 code

--- nowcoder/huawei/test3/test.cpp
+++ nowcoder/huawei/test3/test.cpp
@@ -20,12 +20,20 @@ int main() {
 				if (mode == 0) {
 					cout << s[i];
 				} else {
-					int key = s[i] - '0';
-					int j = 0;
-					while (s[i + j] != '/' && s[i + j] - '0' == key && s[i + j] != '#')
+					int key = s[i];
+					int j = 1;
+					char *pnext = &s[i + 1];
+
+					while (*pnext != '/' &&
+						   *pnext == key &&
+						   *pnext != '#' &&
+						   i + j < n) //不确定c++的string是否自带'\0'结束符号，保险起见还是限制一下防止越界
 					{
+						pnext++;
 						j++;
 					}
+
+					key -= '0';
 					int key_shift = (j-1)%(keymap[key].length());
 					cout << keymap[key][key_shift];
 					i += (j-1);

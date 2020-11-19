12d298c8c1702f79b98465e7737c7386f1eb7e1d
tangfuyuan <362087583@qq.com>
2020-11-19 06:30:22 +0000

    nowcoder 75.lcd: Add new function

--- nowcoder/huawei/75.lcs/test.cpp
+++ nowcoder/huawei/75.lcs/test.cpp
@@ -7,21 +7,15 @@
 
 using namespace std;
 
-int main()
+// 形式1
+void solution1(string & s1, string & s2)
 {
-	auto result = 0;
-	string s1;
-	string s2;
-
-	cin >> s1;
-	cin >> s2;
-
 	int len1 = s1.length() + 1;
 	int len2 = s2.length() + 1;
+	auto result = 0;
 
 	vector<vector<int>> dp(len2, vector<int>(len1, 0));
 
-	// 形式1
 	for (int i = 0; i < len2 - 1; ++i) {
 		for(int j = 0; j < len1 - 1; j++) {
 			if (s2[i] == s1[j])
@@ -39,11 +33,18 @@ int main()
 		cout << endl;
 	}
 	cout << "max = " << result << endl << endl;
+}
 
-	// 形式2
-	int i = 1, j = 1;
-	for (i = 1; i < len2; ++i) {
-		for(j = 1; j < len1; j++) {
+// 形式2
+void solution2(string & s1, string & s2)
+{
+	int len1 = s1.length() + 1;
+	int len2 = s2.length() + 1;
+	auto result = 0;
+	vector<vector<int>> dp(len2, vector<int>(len1, 0));
+
+	for (int i = 1; i < len2; ++i) {
+		for(int j = 1; j < len1; j++) {
 			if (s2[i - 1] == s1[j - 1])
 			{
 				dp[i][j] = dp[i -1][j - 1] + 1;
@@ -59,6 +60,19 @@ int main()
 		cout << endl;
 	}
 	cout << "max = " << result << endl;
+}
+
+int main()
+{
+	auto result = 0;
+	string s1;
+	string s2;
+
+	cin >> s1;
+	cin >> s2;
+
+	solution1(s1, s2);
+	solution2(s1, s2);
 
 	return 0;
 }

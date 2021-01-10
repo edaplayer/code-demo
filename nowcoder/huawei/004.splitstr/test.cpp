7223b4366c6f384eddac16fea268928b3b9b669d
edaplayer <edaplayer@163.com>
2021-01-10 03:09:00 -0800

    huawei: Add 004.splitstr

---
+++ nowcoder/huawei/004.splitstr/test.cpp
@@ -0,0 +1,37 @@
+// 公共子串计算
+// 查找字符串最长公共子串
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+#include <cstdio>
+#include <cstring>
+void print(char s[], int n)
+{
+	for(int i = 0; i<n; i++)
+	{
+		printf("%c", s[i]);
+		if((i+1)%8 == 0)
+			printf("\n");
+	}
+
+	if (n%8 != 0) {
+		int last = 8-n%8;
+		for(int i= 0; i< last; i++)
+			printf("0");
+		printf("\n");
+	}
+}
+
+int main()
+{
+	char s[100];
+	while(scanf("%s", s) != EOF)
+	{
+		int len = strlen(s);
+		print(s, len);
+	}
+	return 0;
+}

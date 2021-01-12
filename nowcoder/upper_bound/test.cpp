a89e7e257bf8960caa5ed5a143a01b294a56d0d6
tangfuyuan <362087583@qq.com>
2021-01-12 10:39:11 +0000

    Fixed upper_bound

--- nowcoder/upper_bound/test.cpp
+++ nowcoder/upper_bound/test.cpp
@@ -7,24 +7,75 @@
 
 #include <iostream>
 #include <vector>
+#include <unistd.h>
+
 using namespace std;
 
-int upper_bound_(int n, int target, vector<int> &a) {
-	if (a.back() < target) return n+1;
-	int l = 0, r = n, m = 0;
-	//结束条件： 左边界小于有边界
+//查找大于或等于t的最小地址
+int lower_bound_(vector<int> &a, int n, int target) {
+	if (a.back() < target) return n + 1;
+	int l = 0, r = n - 1, mid = 0;
+	//结束条件： 左边界不小于右边界
 	while (l < r) {
-		m = (l + r) >> 1; //中点 = (l+r)/2
-		if (a[m] >= target)
-		{
-			r = m; //如果am大于目标值，说明找到了
-		} else l = m + 1;
+		mid = (l + r) >> 1; //中点 = (l+r)/2
+		if (a[mid] < target)
+			l = mid + 1; //如果中点元素小于目标，说明中点左侧数据均不符合，左坐标可以改为中点坐标+1
+		else
+			r = mid; //如果中点元素不小于目标，说明目标值位于中点左侧（包括中点），把右坐标改为中点位置
+		// sleep(1);
 	}
-	return l + 1;
+
+	return l;
+}
+
+//查找大于t的最小地址
+int upper_bound_(vector<int> &a, int size, int target) {
+	if (a.back() <= target) return size + 1;
+
+	int l = 0, r = a.size() - 1, mid;
+	while(l < r) {
+		// cout << "l = " << l << ", r= " << r << endl;
+		mid = (l + r) >> 1; //中点 = (l+r)/2
+		// cout << "mid = " << mid << endl;
+		if(a[mid] <= target)
+			l = mid + 1;//如果中点元素大于或等于目标值，说明目标值在右侧，左坐标移动到中点坐标+1
+		else
+			r = mid;//如果中点元素小于目标值，说明目标值在左侧（包括中点），右坐标左移到中点位置
+		// cout << "l = " << l << ", r= " << r << endl;
+		// sleep(1);
+	}
+	return l;
+}
+
+//二分查找目标值
+int binary_search(vector<int> &a, int n, int target) {
+	if (a.back() < target || a.front() > target) return n + 1; //超出范围，返回n + 1
+
+	int l = 0, r = n - 1, mid = 0;
+	//结束条件： 左边界小于或等于右边界
+	while (l <= r) {
+		// cout << "l = " << l << ", r= " << r << endl;
+		mid = (l + r) / 2 ; //中点 = (l+r)/2
+		// cout << "mid = " << mid << endl;
+		if (a[mid] == target)
+			return mid; //如果中点元素等于目标，说明中点坐标为所求，返回坐标
+		else if (a[mid] < target)
+			l = mid + 1; //如果中点元素小于目标，说明目标值位于中点右侧，左坐标可以改为中点坐标+1
+		else if (a[mid] > target)
+			r = mid - 1; //如果中点元素大于目标，说明目标值位于中点左侧，把右坐标改为中点坐标-1
+	}
+
+	return -1;
 }
 
 int main()
 {
-	vector<int> data = {1,2,4,4,5};
-	cout << upper_bound_(5, 4, data) << endl;
+	vector<int> data = {1,2,3,4,5};
+
+	int res = lower_bound_(data, data.size(), 5);
+	cout << "lower_bound_ 4, position " << res << endl;
+	res = upper_bound_(data, data.size(), 4);
+	cout << "upper_bound_ 4, position " << res << endl;
+	res = binary_search(data, data.size(), 5);
+	cout << "binary_search 4, position " << res << endl;
 }

8b521ee4495a899a6a70445e9362012b8823a211
edaplayer <edaplayer@163.com>
2021-01-17 05:31:59 -0800

    nowcoder: Add demo

---
+++ nowcoder/huawei/demo/test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <deque>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	auto n = 0;
+
+	while (cin >> n) {
+	}
+	return 0;
+}

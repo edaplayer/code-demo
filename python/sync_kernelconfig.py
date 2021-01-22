0a684080a8d665ca8d05e62a312413144f9b8206
tangfuyuan <362087583@qq.com>
2021-01-22 20:21:59 +0800

    python: Add sync_kernelconfig.py

---
+++ python/sync_kernelconfig.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+# File: config.py
+# Function: 同步两个内核配置文件
+# Version: v1.0
+# Author: Edward.Tang
+
+import sys
+import os
+import re
+import collections
+
+def replace_text(file, text, key):
+    with open(file, 'r+') as f:
+        filedata = f.read().replace(text, key)
+        f.seek(0, 0)
+        f.truncate()
+        f.write(filedata)
+
+def get_config(file):
+    keymap = {}
+    keymap = collections.OrderedDict()
+    comment = {}
+    comment = collections.OrderedDict()
+    text = {}
+    text = collections.OrderedDict()
+
+    try:
+        with open(file, 'r', encoding='utf8', errors='ignore') as f:
+            for l in f:
+                line = l.strip()
+                if len(line) > 0:
+                    if line[0] != '#': # 键值项
+                        item = line.split('=')
+                        key = item[0].strip()
+                        if len(item) >= 2 :
+                            value = item[1].strip()
+                        else:
+                            value = ''
+                        keymap[key] = value
+                    else: # 注释项
+                        item = line.strip('#').split('is not')
+                        key = item[0].strip()
+                        if len(item) >= 2 :
+                            value = item[1].strip()
+                            if value == 'set':
+                                comment[key] = value
+
+                    text[key] = l
+
+    except FileNotFoundError as e :
+        print('Error: ' + file + "不存在")
+        exit(1)
+
+    return keymap, comment, text
+
+def sync_config(src, dst):
+    k1, c1, t1 = get_config(src)
+    k2, c2, t2 = get_config(dst)
+
+    for key in k1:
+        if not key in k2:
+            if not key in c2 and not key in t2:
+                with open(dst, 'a') as f:
+                    print(key + '=' + k1[key], file = f) # 添加键值
+            else: # 取消注释
+                replace_text(dst, t2[key], t1[key].strip() + '\n')
+
+        elif k1[key] != k2[key]:
+            replace_text(dst, t2[key], key + '=' + k1[key] + '\n')
+
+    #  注释键值
+    for key in c1:
+        if key in k2:
+            replace_text(dst, t2[key], t1[key].strip() + '\n')
+
+def main():
+    if len(sys.argv) == 3:
+        newconfig = sys.argv[1] #新配置
+        oldconfig = sys.argv[2]    #原始配置文件路径
+        #  replace_text(oldconfig, ' # CONFIG_YYY is not set\n', 'CONFIG_YYY=test\n')
+        sync_config(newconfig, oldconfig)
+    else:
+        print("Need two arguments, please check arguments.")
+
+if __name__ == '__main__':
+    main()
+

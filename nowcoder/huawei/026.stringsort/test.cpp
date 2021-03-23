5ca4722c92bfcf7610ba1380ee9158b0751e8fe6
edaplayer <362087583@qq.com>
2021-03-23 10:46:46 +0800

    nowcoder: Update 026.stringsort

--- nowcoder/huawei/026.stringsort/test.cpp
+++ nowcoder/huawei/026.stringsort/test.cpp
@@ -20,44 +20,61 @@ using namespace std;
 int main()
 {
     string s;
+
     while(getline(cin,s))
     {
         string ans = s;
-		// vector<char> result;
-		string result;
-        vector<vector<char>> V(30);
+        vector<vector<char>> V(26);
+		const char flag = 'a';
+
         for(int i = 0 ; i < s.length() ; i++)
         {
-            if(s[i]>='A'&&s[i]<='Z') //如果是大写字母，放进桶
-            {
-                ans[i]='a';//只是用来标记一下，用什么字母无所谓 标记下标i处理有值
-                V[s[i]-'A'].push_back(s[i]); //进桶
-            }
-            else if(s[i]>='a'&&s[i]<='z') //小写字母，同样要入桶
-            {
-                ans[i]='a';
-                V[s[i]-'a'].push_back(s[i]);//小写字母入桶
-            }
+			char c = s[i];
+			if (isalpha(c)) {
+				ans[i] = flag;
+			} else {
+				continue;
+			}
+
+			if (isupper(c)) {
+                V[c - 'A'].push_back(c);
+			} else if(islower(c)) {
+				V[c - 'a'].push_back(c);
+			}
+
+            // if(s[i]>='A'&&s[i]<='Z') //如果是大写字母，放进桶
+            // {
+                // ans[i]='a';//只是用来标记一下，标记下标i处有值，用什么字母无所谓，但一定不能用非字母! 如果想用非字母，ans必须全部初始化为非标记
+                // V[s[i]-'A'].push_back(s[i]); //进桶
+            // }
+            // else if(s[i]>='a'&&s[i]<='z') //小写字母，同样要入桶
+            // {
+                // ans[i]='a';
+                // V[s[i]-'a'].push_back(s[i]);//小写字母入桶
+            // }
         }
 
 		//把存在的字母排成一行，此时已经有序，且去掉了非字母字符
+		//alpha存储字母数据
+		string alpha;
+
 		for(auto &i : V)
 		{
 			if(i.size()) {
 				for(auto &j : i)
-					result += j;
+					alpha += j;
 			}
 		}
 
-		// cout << "reslult = " << result << endl;
+		// cout << "reslult = " << alpha << endl;
 
 		int i = 0,j = 0;
 		for(int k = 0, j = 0; k < s.length() ; k++)
 		{
-			if(ans[k]=='a') // 该处有值，依次打印result结果
+			if(ans[k] == flag) // 该处有值，依次打印alpha结果
 			{
-				ans[k]= result[j];
-				j++; //取值后result下标要移动
+				ans[k]= alpha[j];
+				j++; //取出字母后alpha下标要移动
 			}
 		}
         cout << ans << endl;

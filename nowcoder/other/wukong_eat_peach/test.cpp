a60ab9e481ac3dd98747ca83ca5d0d1322c863c9
edaplayer <362087583@qq.com>
2021-03-30 15:26:27 +0800

    wukong_eat_peach: Add test2 code

--- nowcoder/other/wukong_eat_peach/test.cpp
+++ nowcoder/other/wukong_eat_peach/test.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <vector>
-#include <stack>
-#include <deque>
+#include <cmath>
 #include <algorithm>
 
 using namespace std;
 
-int main()
+int test1()
 {
 	auto n = 0;
 	vector<int> in;
@@ -45,4 +44,47 @@ int main()
 		}
 	}
 	return 0;
+}
+
+int test2()
+{
+	auto n = 0;
+	vector<int> in;
+	int k = 1;
+
+	while (cin >> n) {
+		in.push_back(n);
+	}
+
+	int time = in.back();
+	in.pop_back();
+	sort(in.begin(), in.end());
+
+	int kmin = in.front();
+	int kmax = in.back();
+
+	int size = in.size();
+	int i = 0;
+	int t = 0;
+
+	for (k = kmin; k <= kmax; ++k) {
+		t = 0;
+		vector<int> temp = in;
+		for (i = 0; i < size; ++i) {
+			// t += ceil(temp[i] / (double)k);
+			t += (temp[i] -1)/k + 1;
+		}
+
+		if(t <= time) {
+			cout << k << endl;
+			break;
+		}
+	}
+	return 0;
+}
+
+int main()
+{
+	// test1();
+	test2();
 }

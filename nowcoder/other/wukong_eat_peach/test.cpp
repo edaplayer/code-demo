32bf1c60a10a6231b8591b6c85ab3eac71e42e89
edaplayer <362087583@qq.com>
2021-03-30 11:38:20 +0800

    nowcoder: Fixed wukong_eat_peach

--- nowcoder/other/wukong_eat_peach/test.cpp
+++ nowcoder/other/wukong_eat_peach/test.cpp
@@ -18,35 +18,23 @@ int main()
 
 	int time = in.back();
 	in.pop_back();
-	// cout << in.back();
 	sort(in.begin(), in.end());
 
 	int kmin = in.front();
 	int kmax = in.back();
 
 	int size = in.size();
-	int j = 0;
+	int i = 0;
 	int t = 0;
-	// cout << time <<endl;
-	// cout << size <<endl;
-	// cout << kmin <<endl;
-	// cout << kmax <<endl;
-	int sum = 0;
-	for(auto &i: in)
-		sum += i;
-	kmax = sum;
-	cout << sum <<endl;
-	for (k = 1; k < kmax; ++k) {
+
+	for (k = kmin; k <= kmax; ++k) {
 		t = 0;
 		vector<int> temp = in;
-		// cout << k <<endl;
-		for (j = 0; j < size;) {
-			// cout << "j=" << j <<endl;
-			if (temp[j] < k) {
-				temp[j] = 0;
-				j++;
+		for (i = 0; i < size;) {
+			if (temp[i] > k) {
+				temp[i] -= k;
 			} else {
-				temp[j] -= k;
+				i++;
 			}
 			t++;
 		}

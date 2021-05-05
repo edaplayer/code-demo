cd773f9f889e0ceb4ae10f605c64c42594f4d567
edaplayer <edaplayer@163.com>
2021-05-05 03:01:17 -0700

    Add gdb

---
+++ gdb/test.c
@@ -0,0 +1,18 @@
+/*************************************************************************
+    > File Name: test.c
+    > Author: Edward.Tang
+    > Mail:   edaplayer@163.com
+    > Created Time: Sun 02 May 2021 09:28:42 AM PDT
+ ************************************************************************/
+#include <stdio.h>
+
+void func()
+{
+	unsigned int i = 0x1234;
+}
+
+int main(int argc, char *argv[])
+{
+	func();
+	return 0;
+}
